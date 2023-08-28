46274feb35cf7741134fc31c236e4e534a578cbc
Liana Mnoyan <liana.mnoyan.93@gmail.com>
2023-08-29 01:00:42 +0400

    homwork1

--- src/chapter3/Average.java
+++ src/chapter3/Average.java
@@ -2,13 +2,14 @@ package chapter3;
 
 public class Average {
     public static void main(String[] args) {
-        double nums[] = {10.1, 11.2, 12.3, 13.4, 14.5};
+        double numbers[] = {10.1, 11.2, 12.3, 13.4, 14.5};
         double result = 0;
         int i;
-        for (i = 0; i < 5; i++) ;
-        result = result + nums[i];
+        for (i = 0; i < 5; i++){
+        result = result + numbers[i];
+        }
         System.out.println("averege equals" + " " + result / 5);
-        //es chgitem inchi hamar chi stacvum grqov espes e.
+        //
 
     }
 
---
+++ src/homework1/FigurePainter.java
@@ -0,0 +1,11 @@
+package homework1;
+
+public class FigurePainter {
+    public static void main(String[]args){
+        int x,y;
+        for (x=0;x<5;x++);
+        for(y=0;y<5;y++)
+    System.out.print('*');
+y=y-1 ;
+    }
+}
