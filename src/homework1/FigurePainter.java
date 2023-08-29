09f4b4bfd17798b9297ead4b941b48081e6ac73a
Liana Mnoyan <liana.mnoyan.93@gmail.com>
2023-08-29 10:49:41 +0400

    homwork1

--- src/homework1/FigurePainter.java
+++ src/homework1/FigurePainter.java
@@ -1,11 +1,36 @@
 package homework1;
 
 public class FigurePainter {
-    public static void main(String[]args){
-        int x,y;
-        for (x=0;x<5;x++);
-        for(y=0;y<5;y++)
-    System.out.print('*');
-y=y-1 ;
+    //first Example
+    public static void main(String[] args) {
+
+        for (int x = 0; x < 5; x++) {
+            for (int y = 0; y <= x; y++) {
+
+                System.out.print("*");
+            }
+            System.out.println();
+
+
+        }
+
     }
 }
+
+
+//second Example
+public class FigurePainter1 {
+    public static void main(String[] args) {
+        for (int x = 0; x < 5; x++) {
+            for (int y = 0; y >= x; y--) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+
+}
+
+
+
